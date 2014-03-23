3078aa77788dd46ced25b9119ed1b05207028530
Jesse Millar <jesse@jessemillar.com>
2014-03-23 12:04:20 -0600

    Image loading and drawing and canvas resizing works.

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
-var skin // The image we'll draw to the canvas
+var blankColor = '#FFFFFF'
+var boxColor = '#FF0000'
+var canvasWidth = 300
+var canvasHeight = 300
+
+var skin = new Image() // The image we'll draw to the canvas
+
+var loop = setInterval(drawCanvas, 1000 / 60)
 
 var dropper = document.getElementById('dropper')
 	dropper.addEventListener('dragover', handleDragOver, false)
 	dropper.addEventListener('drop', handleDrop, false)
+
+var dom = document.getElementById('canvas')
+var ctx = dom.getContext('2d')
 
 function handleDragOver(event)
 {
@@ -18,6 +28,40 @@ function handleDrop(event)
 	event.stopPropagation()
 	event.preventDefault()
 
-	skin = new Image()
-        skin.src = event.dataTransfer.files[0]
+	var reader = new FileReader()
+
+	reader.onload = function(file)
+	{
+		skin.src = file.target.result
+		document.getElementById('hack').src = skin.src
+		document.getElementById('dropper').style.display = 'none'
+
+		canvasWidth = document.getElementById('hack').offsetWidth
+		canvasHeight = document.getElementById('hack').offsetHeight
+
+		dom.width = canvasWidth
+		dom.height = canvasHeight
+
+		console.log(dom.width, dom.height)
+	}
+
+    reader.readAsDataURL(event.dataTransfer.files[0])
+}
+
+function drawCanvas()
+{
+	blank()
+	ctx.drawImage(skin, 0, 0)
+}
+
+function blank()
+{
+	ctx.fillStyle = blankColor
+	ctx.fillRect(0, 0, canvasWidth, canvasHeight)
+}
+
+function drawRectangle(x, y, width, height)
+{
+    l.ctx.fillStyle = boxColor
+    l.ctx.fillRect(Math.round(x), Math.round(y), width, height)
 }
